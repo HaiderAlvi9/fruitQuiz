1b871e312ff5a646b39daf91040558c7ccdefda3
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2022-11-16 02:43:32 +0500

    image Error

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.view.View;
 import android.widget.ImageView;
 import android.os.Bundle;
 import android.widget.Button;
-public class MainActivity extends AppCompatActivity {
-    Button b1;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b1,b2,b3,b4;
     ImageView iv;
-    boolean flag;
-    int[] images ={R.drawable.ap,R.drawable.ba,R.drawable.gr,R.drawable.gv
-    };
-    int i=0;
+  int[] images ={R.drawable.ap,R.drawable.av,R.drawable.ba,R.drawable.gr,R.drawable.gv};
+    int answer=0;
+    String [] a={"apple","avocado","banana","grapes","guava"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +23,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         iv=findViewById(R.id.img1);
         b1=findViewById(R.id.btn1);
+        b2=findViewById(R.id.btn2);
+        b3=findViewById(R.id.btn3);
+         b4=findViewById(R.id.btn4);
+         b1.setOnClickListener(this);
+        generateRandom();
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                iv.setImageResource(images[i]);
-                i++;
-                if(i==4)
-                    i=0;
-            }
-        });
 
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btn1){
+          generateRandom();
+        }
+    }
+   public void generateRandom(){
+
+        Random rnd = new Random();
+        int var= rnd.nextInt(5);
+
+        b2.setText(a[var]);
+      iv.setImageResource(images[var]);
+
+             b4.setText(""+var);
+
+    }
+
 }
